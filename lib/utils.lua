477e1e7fb0e2f077bc914f66139fb2c47083bbc4
Anthony Loukinas <anthony.loukinas@gmail.com>
2021-11-26 18:09:13 -0500

    updates

--- lib/utils.lua
+++ lib/utils.lua
@@ -6,8 +6,9 @@
 function log(message, level, script)
     if level == nil then
         level = "INFO"
+        print("[" .. script .. "] [INFO] " .. message)
     elseif level == "INFO" then
-        print("[" .. script .. "] " .. message)
+        print("[" .. script .. "] [INFO] " .. message)
     elseif level == "WARN" then
         print("[" .. script .. "] [WARN] " .. message)
     elseif level == "ERROR" then
