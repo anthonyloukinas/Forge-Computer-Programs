a0abed504ce27e889b4094504f27ac9fdc35e0e4
Anthony Loukinas <anthony.loukinas@gmail.com>
2021-11-27 00:41:37 -0500

    Updates to manager.lua

--- scripts/reactor/manager.lua
+++ scripts/reactor/manager.lua
@@ -8,20 +8,86 @@ debugging = false
 -- Imports
 local utils = require("../../lib/utils")
 
+
 -- Variables
 local script_name = "manager"
 local version = "1.0.0"
 local author = "Anthony Loukinas"
 local description = "Manages BiggerReactor/ExtremeReactor's energy efficiency."
 
+-- Config
+local config = {}
+config.energy_toggle_percent = 80
+
+-- Script Variables
+local reactor = {}
+
 
 -- Functions
 function init()
+    monitors = { peripheral.find("monitor") }
+
+    -- Allow 5 attempts at discovering the RefinedStorage network
+    local loopI = 0
+    while loopI ~= 5 do
+
+        -- BiggerReactors v0.5.1 (MC-1.16.5)
+        if peripheral.find("BiggerReactors_Reactor") ~= nil then
+            reactors = { peripheral.find("BiggerReactors_Reactor") }
+            break
+        end
+
+        sleep(1)
+        loopI = loopI + 1
+    end
+
+    if monitors[1] == nil then
+        log("No monitors detected.", "error", script_name)
+    end
+
+    if reactors[1] == nil then
+        log("No reactors detected.", "error", script_name)
+        error("No reactors detected, exiting script")
+    end
+
+    reactor = reactors[1]
+
     log(script_name .. " v" .. version .. " by " .. author .. " loaded.", "info", script_name)
 end
 
 function main()
-    
+    while true do
+        
+        local is_active = reactor.active()
+        local energy_stored = reactor.battery().stored()
+        local energy_capacity = reactor.battery().capacity()
+        local energy_percentage = math.floor((energy_stored / energy_capacity) * 100)
+
+        -- Reactor Monitor
+        -- local monitor_text = "Reactor: " .. (is_active and "Active" or "Inactive") .. "\n"
+        -- monitor_text = monitor_text .. "Energy Stored: " .. energy_stored .. " RF\n"
+        -- monitor_text = monitor_text .. "Energy Capacity: " .. energy_capacity .. " RF\n"
+        -- monitor_text = monitor_text .. "Energy Percentage: " .. energy_percentage .. "%\n"
+
+        -- TODO Check fuel
+
+        -- If energy percentage is below the config value, activate the reactor
+        if energy_percentage < config.energy_toggle_percent then
+            if not is_active then
+                log("Reactor is inactive, activating.", "info", script_name)
+                reactor.setActive(true)
+            end
+        -- Else if energy percentage is above the config value, deactivate the reactor
+        else
+            if not is_active then
+                log("Reactor is active, deactivating.", "info", script_name)
+                reactor.setActive(false)
+            end
+        end
+
+        log("Sleeping 5 seconds", "debug", script_name)
+        sleep(5)
+    end
 end
 
 
