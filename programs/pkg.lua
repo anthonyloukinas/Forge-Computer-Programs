eb0aad8e06106b7282e6762bd1f764e418b7e4a8
Anthony Loukinas <anthony.loukinas@gmail.com>
2021-11-28 00:35:32 -0500

    updates

--- programs/pkg.lua
+++ programs/pkg.lua
@@ -57,8 +57,8 @@ function list()
     
     local packages = http.get("https://raw.githubusercontent.com/anthonyloukinas/Forge-Computer-Programs/main/package-list.json").readAll()
     local data = json.decode(packages)
-    for i = 1, #data do
-        print(data[i].name)
+    for i = 1, #data.packages do
+        print(data.packages[i].name)
     end
 
 end
